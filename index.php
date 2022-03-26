d60ebfb8cf467fd7e79b37212e5edf01f5cf20f4
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-03-26 22:58:12 +0600

    first commit about how php works

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer-Index</title>
+</head>
+<body>
+    <!-- Basic HTML Part -->
+    <h2>Hello World</h2>
+    -- --  -- 
+    <h1>This is PHP </h1>
+
+    <!-- Basic PHP Part -->
+
+    <?php
+    //  printing HTML using PHP
+        echo "Hello PHP " ;
+        echo '<br/>' ;
+        echo 'Second Line';
+        echo '<br/>';
+
+    ?>
+
+    <?php
+        // declare variable
+        $name ='MK Hridoy';
+        $age='25';
+
+        //print the variabele
+        echo $name;
+        echo '<h1>My Name Is : '.$name.'</h1>';
+        echo '<h2>I am '.$age.' years old </h2> ';
+    ?>
+</body>
+</html>
